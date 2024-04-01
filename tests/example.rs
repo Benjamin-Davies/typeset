d82374365b729ec256cb5ee1b359d21b249b7d37
Benjamin Davies <bentendavies@gmail.com>
2024-04-01 22:44:04 +1300

    Add an integration test

---
+++ tests/example.rs
@@ -0,0 +1,104 @@
+use std::{collections::BTreeMap, fs};
+
+use glam::vec2;
+use typeset::{
+    document::{Block, Document, Inline, Style, TextAlign, TextBlock},
+    font::Font,
+    pdf::{
+        page::{PageBuilder, PAGE_HEIGHT, PAGE_WIDTH},
+        PDFBuilder,
+    },
+    text_layout::layout_document,
+};
+
+#[test]
+fn test_example() {
+    let font = Font::default();
+    let bold_font = Font::new(include_bytes!(concat!(
+        env!("OUT_DIR"),
+        "/noto-serif/NotoSerif-Bold.ttf"
+    )));
+    let italic_font = Font::new(include_bytes!(concat!(
+        env!("OUT_DIR"),
+        "/noto-serif/NotoSerif-Italic.ttf"
+    )));
+    let text = include_str!("../examples/lorem_ipsum.txt");
+
+    let style = Style {
+        font: &font.ps_name,
+        font_size: 12.0,
+    };
+    let mut blocks = vec![
+        Block::Text(TextBlock {
+            inlines: vec![Inline {
+                style: Style {
+                    font_size: 2.0 * style.font_size,
+                    ..style
+                },
+                text: "Hello, World!",
+            }],
+            align: TextAlign::Left,
+        }),
+        Block::Text(TextBlock {
+            inlines: vec![
+                Inline {
+                    style,
+                    text: "Regular, ",
+                },
+                Inline {
+                    style: Style {
+                        font: &bold_font.ps_name,
+                        ..style
+                    },
+                    text: "bold, ",
+                },
+                Inline {
+                    style: Style {
+                        font: &italic_font.ps_name,
+                        ..style
+                    },
+                    text: "or italic?",
+                },
+            ],
+            align: TextAlign::Left,
+        }),
+    ];
+    for (line, align) in text.lines().zip([
+        TextAlign::Justify,
+        TextAlign::Left,
+        TextAlign::Center,
+        TextAlign::Right,
+        TextAlign::Justify,
+    ]) {
+        blocks.push(Block::Text(TextBlock {
+            inlines: vec![Inline { style, text: line }],
+            align,
+        }));
+    }
+
+    let mut fonts = BTreeMap::new();
+    fonts.insert(&*font.ps_name, &font);
+    fonts.insert(&*bold_font.ps_name, &bold_font);
+    fonts.insert(&*italic_font.ps_name, &italic_font);
+
+    let document = Document {
+        blocks,
+        fonts,
+        page_size: vec2(PAGE_WIDTH, PAGE_HEIGHT),
+        margin: 72.0,
+    };
+
+    let pages = layout_document(&document);
+
+    let mut pdf_builder = PDFBuilder::new();
+    for page in pages {
+        let page = PageBuilder::new().text(&page.lines).build();
+        pdf_builder.page(&page);
+    }
+    pdf_builder.catalog(&document.fonts);
+    let content = pdf_builder.build();
+
+    fs::write("target/output.pdf", &content).unwrap();
+
+    assert_eq!(content, include_bytes!("../examples/lorem_ipsum.pdf"));
+}
